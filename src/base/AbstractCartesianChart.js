f5aadbcf32f705c4bb4da1e83ccd9144c1c0a043
Xing Yun <ink.xing.yun@gmail.com>
2017-08-02 21:16:19 -0400

    move tooltip from core

--- src/base/AbstractCartesianChart.js
+++ src/base/AbstractCartesianChart.js
@@ -3,10 +3,12 @@ import {
     mergeBase,
     GRADIENT,
     DISTINCT,
-    CATEGORICAL
+    CATEGORICAL,
+    uuid
 } from 'vizart-core';
 import TooltipTpl from './tooltip-tpl';
 import './tooltip.css';
+import { select, mouse } from 'd3-selection';
 
 class AbstractCartesianChart extends AbstractChart {
     constructor(canvasId, _userOptions) {
@@ -44,6 +46,12 @@ class AbstractCartesianChart extends AbstractChart {
 
     render(_data) {
         super.render(_data);
+
+        this._tooltip = select(this._containerId)
+            .append("div")
+            .attr('id', 'tooltip-' + uuid())
+            .attr('class', 'vizart-tooltip')
+            .style("opacity", 0);
     }
 
     sort(_accessor, direction) {
@@ -76,6 +84,42 @@ class AbstractCartesianChart extends AbstractChart {
             .replace("{{name}}", this._getMetric().name)
             .replace("{{value}}", this._getMetricVal(d))
             .replace("{{borderStroke}}", this._c(d));
+    }
+
+    //todo not a nice way to use 'this', need to improve
+    _bindTooltip(_selector, polar = false) {
+        let that = this;
+
+        function _mouseMove(d) {
+            that._tooltip
+                .transition()
+                .duration(that._options.tooltip.duration)
+                .style("opacity", 1);
+
+            let coordinates = mouse(this);
+            let x = coordinates[0];
+            let y = coordinates[1];
+
+            if (polar === true) {
+                that._tooltip.style("left", (x + (that._options.chart.width / 2)) + "px")
+                    .style("top", (y + (that._options.chart.height / 2) +  90) + "px")
+                    .html(that._getTooltipHTML(d));
+            } else {
+                that._tooltip.style("left", x < 40 ? x : (x - 22) + "px")
+                    .style("top", y < 40 ? y + 34 : (y - 34) + "px")
+                    .html( that._getTooltipHTML(d));
+            }
+        };
+
+        function _mouseOut() {
+            that._tooltip.transition()
+                .duration(that._options.tooltip.duration)
+                .style("opacity", 0)
+        }
+
+        _selector
+            .on("mousemove", _mouseMove)
+            .on("mouseout", _mouseOut);
     }
 
 }
