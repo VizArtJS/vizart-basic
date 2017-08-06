054ebe2504446f21bc6deb9c7705dcb74926c284
Xing Yun <ink.xing.yun@gmail.com>
2017-08-06 00:52:51 -0400

    high order functions

--- src/data/coordinates/metric-stacked.js
+++ src/data/coordinates/metric-stacked.js
@@ -3,10 +3,24 @@ import { max, min } from 'd3-array';
 import tickRange from './update-scale/ticks';
 import Stacks from '../constant/stack-methods';
 
+const mapY = d => d.values.map(e=> e.y);
+
+/**
+ * get max/min of the nestedData
+ * 
+ * @param _nested nested data
+ * @param _oper min/max
+ * @returns {*} max/min of the nestedData
+ */
+const mapNested = (_nested, _oper)=> {
+    return _oper(_nested.map(d=> _oper(mapY(d))));
+}
+
 const getStackedMetricScale = (_nestedData, _options)=> {
     let _tickedRange;
     let minY = 0;
-    let maxY = max(_nestedData.map( d=> max(d.values.map(d => d.y))));
+    console.log(mapNested(_nestedData, max));
+    let maxY = mapNested(_nestedData, max);
 
     if (_options.plots.stackMethod === Stacks.Expand ) {
         if (maxY === 1) {
@@ -15,7 +29,7 @@ const getStackedMetricScale = (_nestedData, _options)=> {
             _tickedRange = tickRange([0, maxY], _options.yAxis[0].ticks, _options.yAxis[0].tier);
         }
     } else {
-        minY = min(_nestedData.map( d=> min(d.values.map(d=> d.y))));
+        minY = mapNested(_nestedData, min);
         _tickedRange = tickRange([minY, maxY], _options.yAxis[0].ticks,_options.yAxis[0].tier);
     }
 
