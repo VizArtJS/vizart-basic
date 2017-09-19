1687e5e4011e91a180b1d60a2ed63762d86f0cf3
Xing Yun <ink.xing.yun@gmail.com>
2017-09-19 16:36:21 +0800

    pie works

--- src/modal/Pie/index.js
+++ src/modal/Pie/index.js
@@ -1,12 +1,19 @@
-import { check, Globals } from 'vizart-core';
-import { pie, arc } from 'd3-shape';
+import {
+    pie,
+    arc
+} from 'd3-shape';
 import { format } from 'd3-format';
 import { sum } from 'd3-array';
-import { interpolate } from 'd3-interpolate';
+import { interpolateArray } from 'd3-interpolate';
 import { transition } from 'd3-transition';
+import { timer } from 'd3-timer';
+import { mouse } from 'd3-selection';
+import { easeCubic } from 'd3-ease';
+import { check, Globals } from 'vizart-core';
+
 
 import createCartesianOpt from '../../options/createCartesianOpt';
-import { AbstractBasicCartesianChart } from '../../base';
+import AbstractCanvasChart from '../../canvas/AbstractCanvasChart';
 import TooltipTpl from '../../base/tooltip-tpl';
 
 import {
@@ -20,17 +27,45 @@ import {
     limitSliceValues
 } from './helper';
 
+const drawCanvas = (context, state, opt)=> {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    const radius = Math.min(opt.chart.innerWidth, opt.chart.innerHeight) / 2;
+    const innerArc = arc()
+        .outerRadius(radius * 0.8)
+        .innerRadius(() => opt.plots.isDonut ? radius * opt.plots.innerRadiusRatio : 0)
+        .context(context);
+
+    const outerArc = arc()
+        .innerRadius(radius * 0.9)
+        .outerRadius(radius * 0.9)
+        .context(context);
+
+    const _pie = pie()
+        .sort(null)
+        .value(d=>d.y);
+
+    const slices = _pie(state);
+
+    context.save();
+    context.translate(opt.chart.width / 2, opt.chart.height / 2);
+
+    for (const s of slices) {
+        context.beginPath();
+        innerArc(s);
+        context.fillStyle = s.data.c;
+        context.fill();
+    }
+
+    context.restore();
+
+}
 
 const percentFormat = format(".00%");
 
 const DefaultOptions = {
     chart: {
         type: 'pie'
-    },
-    animation: {
-        duration: {
-            slice: 1000,
-        }
     },
     plots: {
         othersTitle: 'Others',
@@ -43,331 +78,107 @@ const DefaultOptions = {
     }
 };
 
-class Pie extends AbstractBasicCartesianChart {
+class Pie extends AbstractCanvasChart {
     constructor(canvasId, _userOptions) {
         super(canvasId, _userOptions);
-
-        this.sliceGroup;
-        this.labelGroup;
-        this.lineGroup;
-        this.linkGroup;
-
-        this.total = 0;
-        this.consecutiveSmalls = false;
-        this._limitSliceValues = limitSliceValues.bind(this);
-        this._mergeWithFirstEqualZero = mergeWithFirstEqualZero.bind(this);
-
-        this._key = d=> d.data[this._getDimension().accessor];
-        //override
-        this._c =(d, i) => {
-            if (d.color) {
-                return d.color;
-            }
-
-            switch (this._options.color.type){
-                case Globals.ColorType.CATEGORICAL:
-                    return this._color(this._getDimensionVal(d.data));
-                case Globals.ColorType.GRADIENT:
-                case Globals.ColorType.DISTINCT:
-                    return this._color(this._getMetricVal(d.data));
-                default:
-                    return this._color(this._getMetricVal(d.data));
-            }
-        }
-        this._p = (d)=> {
-            let pct = this._getMetricVal(d.data) / this.total;
-            return pct < this.minPct && this.consecutiveSmalls
-                ? ''
-                : this._getDimensionVal(d.data) + ': ' + percentFormat(pct);
-        };
     }
 
-    render(_data) {
-        super.render(_data);
 
-        this._svg.attr("transform", "translate("
-            + (this._options.chart.innerWidth / 2 + this._options.chart.margin.left)
-            + ","
-            + (this._options.chart.innerHeight / 2 + this._options.chart.margin.top)
-            + ")");
+    _animate() {
+        const Duration = this._options.animation.duration.update;
 
-        this.sliceGroup = this._svg.append("g").attr("class", "slices");
-        this.labelGroup = this._svg.append("g").attr("class", "labels");
-        this.lineGroup = this._svg.append("g").attr("class", "lines");
-        this.linkGroup = this._svg.append("g").attr('class', 'link-group');
-
-        this.radius = Math.min(this._options.chart.innerWidth, this._options.chart.innerHeight) / 2;
         this.minPct = this._options.slice.labelMinPercentage > 0
             ? this._options.slice.labelMinPercentage
             : 0.01;
 
-        this.update();
-    }
 
-    update() {
+        const initialState = this.previousState
+            ? this.previousState
+            : this._data.map(d=>{
+
+                return {
+                    x: this._x(d),
+                    y: this._frontCanvas.node().height,
+                    c: this._c(d),
+                    alpha: 0,
+                    data: d
+                }
+            });
+
+        const finalState = this._data.map(d=>{
+            console.log(this._c(d));
+            return {
+                x: this._x(d),
+                y: this._y(d),
+                c: this._c(d),
+                alpha: this._options.plots.opacity,
+                data: d
+            }
+        });
+
+        // cache finalState as the initial state of next animation call
+        this.previousState = finalState;
+
+        const interpolateParticles = interpolateArray(initialState, finalState);
+
         let that = this;
+        const batchRendering = timer( (elapsed)=> {
+            const t = Math.min(1, easeCubic(elapsed / Duration));
 
-        super.update();
+            drawCanvas(that._frontContext,
+                interpolateParticles(t),
+                that._options);
 
-        let _arc = arc()
-            .outerRadius(this.radius * 0.8)
-            .innerRadius(() => {
-                return this._options.plots.isDonut ? this.radius * this._options.plots.innerRadiusRatio : 0;
-            });
+            if (t === 1) {
+                batchRendering.stop();
 
-        this.outerArc = arc()
-            .innerRadius(this.radius * 0.9)
-            .outerRadius(this.radius * 0.9);
 
-        let _pie = pie()
-            .sort(null)
-            .value(this._getMetricVal);
+                /**
+                 * callback for when the mouse moves across the overlay
+                 */
+                function mouseMoveHandler() {
+                    // get the current mouse position
+                    const [mx, my] = mouse(this);
+                    const QuadtreeRadius = 100;
+                    // use the new diagram.find() function to find the Voronoi site
+                    // closest to the mouse, limited by max distance voronoiRadius
+                    const closest = that._voronoi.find(mx, my, QuadtreeRadius);
 
-        let data1 = this._limitSliceValues(this.total);
-        let data0 = this._svg.select('.slices')
-            .selectAll("path.slice")
-            .data().map((d) => { return d.data });
-        if (data0.length == 0) data0 = data1;
-        let was = this._mergeWithFirstEqualZero(data1, data0);
-        let is = this._mergeWithFirstEqualZero(data0, data1);
+                    if (closest) {
+                        that._tooltip
+                            .html( that._getTooltipHTML(closest.data.data))
+                            .transition()
+                            .duration(that._options.animation.tooltip)
+                            .style("opacity", 1)
+                            .style("left", closest[0] + "px")
+                            .style("top", closest[1] + "px");
 
-        /* ------- SLICE ARCS -------*/
-
-        let slice = this.sliceGroup.selectAll("path.slice")
-            .data(_pie(was), this._key);
-
-        slice.enter()
-            .append("path")
-            .attr("class", "slice")
-            .each(function(d) { this._current = d; })
-            .style("fill", this._c)
-            .style('stroke-width', 2);
-
-        slice = this.sliceGroup.selectAll("path.slice")
-            .data(_pie(is), this._key);
-
-        slice
-            .transition().duration(this._options.animation.duration.slice)
-            .attrTween("d", function (d) {
-                var _interpolate = interpolate(this._current, d);
-                var _this = this;
-                return function (t) {
-                    _this._current = _interpolate(t);
-                    return _arc(_this._current);
-                };
-            });
-
-        slice = this.sliceGroup.selectAll("path.slice")
-            .data(_pie(data1), this._key);
-
-        slice
-            .exit().transition().delay(this._options.animation.duration.slice).duration(0)
-            .remove();
-
-        /* ------- TEXT LABELS -------*/
-
-        let text = this.labelGroup.selectAll("text")
-            .data(_pie(was), this._key);
-
-        text.enter()
-            .append("text")
-            .attr('dx', "0px")
-            .attr("dy", "0px")
-            .style('font-size', '12px')
-            .style("opacity", 0)
-            .text(this._p)
-            .each(function (d) {
-                this._current = d;
-            });
-
-        text = this.labelGroup.selectAll("text")
-            .data(_pie(is), this._key);
-
-        text.transition().duration(this._options.animation.duration.slice)
-            .style("opacity", (d)=> {
-                return this._getMetricVal(d.data) == 0 ? 0 : 1;
-            })
-            .attrTween("transform", function (d) {
-                let _interpolate = interpolate(this._current, d);
-                let _this = this;
-                return function (t) {
-                    let d2 = _interpolate(t);
-                    _this._current = d2;
-
-                    let pos = getLabelPosition(that.outerArc, d2);
-
-                    return "translate(" + pos + ")";
-                };
-            })
-            .styleTween("text-anchor", function (d) {
-                let _interpolate = interpolate(this._current, d);
-                return function (t) {
-                    let d2 = _interpolate(t);
-                    return midAngle(d2) < Math.PI ? "start" : "end";
-                };
-            })
-            .text(this._p);
-
-        text = this.labelGroup.selectAll("text")
-            .data(_pie(data1), this._key);
-
-        text
-            .exit().transition().delay(this._options.animation.duration.slice)
-            .remove();
-
-        /* ------- SLICE TO TEXT POLYLINES -------*/
-
-        let polyline = this.lineGroup.selectAll("polyline")
-            .data(_pie(was), this._key);
-
-        polyline.enter()
-            .append("polyline")
-            .style("opacity", 0)
-            .style('stroke', this._c)
-            .style('stroke-width', 2)
-            .style('fill', 'none')
-            .each(function (d) {
-                this._current = d;
-            });
-
-        polyline = this.lineGroup.selectAll("polyline")
-            .data(_pie(is), this._key);
-
-        polyline.transition().duration(this._options.animation.duration.slice)
-            .attr('fill', this._c)
-            .style("opacity",  (d)=> {
-                return this._getMetricVal(d.data) == 0 ? 0 : .5;
-            })
-            .attrTween("points", function (d) {
-                this._current = this._current;
-                let _interpolate = interpolate(this._current, d);
-                let _this = this;
-
-                return function (t) {
-                    let d2 = _interpolate(t);
-                    _this._current = d2;
-
-                    let pct = that._getMetricVal(d.data) / that.total;
-
-                    if (pct < that.minPct && that.consecutiveSmalls) {
-                        return [];
                     } else {
-                        let c = _arc.centroid(d2);
-                        let x = c[0];
-                        let y = c[1];
-
-                        let starting = [];
-                        // pythagorean theorem for hypotenuse
-                        let h = Math.sqrt(x * x + y * y);
-
-                        starting[0] = x / h * that.radius * 0.8;
-                        starting[1] = y / h * that.radius * 0.8;
-
-                        let pos = getLinePosition(that.outerArc, d2);
-
-                        return [starting, pos];
+                        that._tooltip
+                            .transition()
+                            .duration(that._options.animation.tooltip)
+                            .style("opacity", 0)
                     }
-                };
-            });
+                }
 
-        polyline = this.lineGroup.selectAll("polyline")
-            .data(_pie(data1), this._key);
+                function mouseOutHandler() {
+                    that._tooltip.style("opacity", 0)
+                }
 
-        polyline
-            .exit().transition().delay(this._options.animation.duration.slice)
-            .remove();
+                that._frontCanvas.on('mousemove', mouseMoveHandler);
+                that._frontCanvas.on('mouseout', mouseOutHandler);
 
+                // draw hidden in parallel;
+                // drawPoints(that._hiddenContext,
+                //     interpolateParticles(t),
+                //     that._options, true);
 
-        /* ------- Link Nodes -------*/
-
-        let linkNode = this.linkGroup.selectAll(".link-node")
-            .data(_pie(was), this._key);
-
-        linkNode.enter()
-            .append("circle")
-            .attr('class', 'link-node')
-            .attr("r", 6)
-            .style("opacity", 0)
-            .attr('fill', this._c)
-            .attr('stroke', 'white')
-            .attr('stroke-width', '2px')
-            .each(function (d) {
-                this._current = d;
-            });
-
-        linkNode = this.linkGroup.selectAll(".link-node")
-            .data(_pie(is), this._key);
-
-        linkNode.transition().duration(this._options.animation.duration.slice)
-            .style("opacity", (d)=> {
-                return this._getMetricVal(d.data) == 0 ? 0 : 1;
-            })
-            .attrTween("transform", function (d) {
-                //let c = arc.centroid(d),
-                //    x = c[0],
-                //    y = c[1],
-                //// pythagorean theorem for hypotenuse
-                //    h = Math.sqrt(x*x + y*y);
-                //return "translate(" + (x/h * radius) +  ',' +
-                //    (y/h * radius) +  ")";
-
-                this._current = this._current;
-                let _interpolate = interpolate(this._current, d);
-                let _this = this;
-                return function (t) {
-                    let d2 = _interpolate(t);
-                    _this._current = d2;
-
-                    let c = that.outerArc.centroid(d2);
-                    let x = c[0];
-                    let y = c[1];
-                    // pythagorean theorem for hypotenuse
-                    let h = Math.sqrt(x * x + y * y);
-                    return "translate(" + (x / h * that.radius * 0.8) + ',' +
-                        (y / h * that.radius * 0.8) + ")";
-                    //return "translate("+  +")";
-                };
-            });
-
-
-        linkNode = this.linkGroup.selectAll(".link-node")
-            .data(_pie(data1), this._key);
-
-        linkNode
-            .exit().transition().delay(this._options.animation.duration.slice)
-            .remove();
-
-        this._bindTooltip(this.sliceGroup.selectAll("path.slice"), true);
+                that._listeners.call('rendered');
+            }
+        });
     }
 
 
-    transitionColor(colorOptions) {
-        super.transitionColor(colorOptions);
-
-        let maxTicks = this._options.xAxis.ticks > 0 ? this._options.xAxis.ticks : 31;
-        let maxSlices = this._data.length > 0 && this._data.length < maxTicks
-            ? this._data.length
-            : maxTicks;
-
-        let _trans = transition()
-            .duration(this._options.animation.duration.color)
-            .delay((d, i) => {
-                return i / maxSlices * this._options.animation.duration.color;
-            });
-
-        this.sliceGroup.selectAll("path.slice")
-            .transition(_trans)
-            .style("fill", this._c);
-
-
-        this.linkGroup.selectAll(".link-node")
-            .transition(_trans)
-            .style("fill", this._c);
-
-        this.lineGroup.selectAll("polyline")
-            .transition(_trans)
-            .style("stroke", this._c);
-    };
 
     donut(isDonut = false) {
         if (isDonut === false) {
@@ -382,7 +193,7 @@ class Pie extends AbstractBasicCartesianChart {
 
     data(_data) {
         if (check(_data) === true) {
-            this.total = sum(_data, (d) => { return this._getMetricVal(d); });
+            this.total = sum(_data, d => this._getMetricVal(d));
         }
 
         super.data(_data);
@@ -391,14 +202,6 @@ class Pie extends AbstractBasicCartesianChart {
     createOptions(_userOpt) {
         return createCartesianOpt(DefaultOptions, _userOpt);
     };
-
-    _getTooltipHTML(d) {
-        return TooltipTpl
-            .replace("{{header}}", this._getDimensionVal(d.data))
-            .replace("{{name}}", this._getMetric().name)
-            .replace("{{value}}", this._getMetricVal(d.data))
-            .replace("{{borderStroke}}", this._c(d));
-    }
 
     _provideColor() {
         // pie's other slice may contain value out of range
