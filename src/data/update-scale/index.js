f0feab97a2a0e850e39952498785d9ed4b2e434b
Xing Yun <ink.xing.yun@gmail.com>
2017-09-23 14:43:33 +0800

    no series check on cartesian scales

--- src/data/update-scale/index.js
+++ src/data/update-scale/index.js
@@ -1,11 +1,9 @@
 import updateDimensionScale from './update-dim-scale';
-import updateSeriesScale from './update-s-scale';
 import updateMetricScale from './update-metric-scale';
 
-const updateOptionScales = (_data, _options)=> {
-    updateDimensionScale(_data, _options);
-    updateSeriesScale(_data, _options);
-    updateMetricScale(_data, _options);
+const updateOptionScales = (data, opt)=> {
+    updateDimensionScale(data, opt);
+    updateMetricScale(data, opt);
 }
 
 export default updateOptionScales;
