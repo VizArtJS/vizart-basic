b773ad46a81a843f22c9444eef72717667e4d2af
Xing Yun <ink.xing.yun@gmail.com>
2017-10-19 21:48:05 -0400

    display metric on horizontal bar

---
+++ src/modal/HorizontalBar/draw-metric-label.js
@@ -0,0 +1,23 @@
+const drawMetricLabel = (context, node, opt)=> {
+    if (node.attr('metric')) {
+        context.save();
+
+        context.translate(node.attr('x'), node.attr('y'));
+
+        context.textBaseline = 'middle';
+        context.fillStyle = opt.plots.metricLabel.color;
+        context.lineWidth = 4;
+
+        if (+node.attr('metric') < 0) {
+            context.textAlign = "end";
+            context.fillText(node.attr('metric'), -opt.plots.metricLabel.offset, node.attr('height') / 2);
+        } else {
+            context.textAlign = "start";
+            context.fillText(node.attr('metric'), (+node.attr('width') + opt.plots.metricLabel.offset) , +node.attr('height') / 2);
+        }
+
+        context.restore();
+    }
+}
+
+export default drawMetricLabel;
