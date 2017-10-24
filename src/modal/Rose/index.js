68f7fc93135996dc19d8e173536d28a38e56cb05
Xing Yun <ink.xing.yun@gmail.com>
2017-10-23 21:31:40 -0400

    refactor rose

--- src/modal/Rose/index.js
+++ src/modal/Rose/index.js
@@ -1,12 +1,11 @@
 import {scaleLinear, scaleOrdinal} from 'd3-scale';
-import { range } from 'd3-array';
 import {AbstractStackedCartesianChart} from '../../base';
 import createCartesianStackedOpt from '../../options/createCartesianStackedOpt';
 import animateStates from "./tween-states";
 import getRadius from '../Corona/get-radius';
+import getInitialState from './get-initial-state';
+import drawBloomingRose from './draw-blooming-rose';
 import {Stacks} from '../../data';
-import drawCanvas from './draw-canvas';
-import groupBy from 'lodash-es/groupBy';
 
 const RoseOpt = {
     chart: {
@@ -54,30 +53,6 @@ class Rose extends AbstractStackedCartesianChart {
             .domain([0, sliceNum])
             .range([0, 2 * Math.PI]);
 
-        const initialState = this.previousState
-            ? this.previousState
-            : this._getDimension().values.map((d, i) => {
-            let array = this._data.nested.map(e=> {
-                return {
-                    key: e.key,
-                    s: e.key,
-                    c: c(e.key),
-                    alpha: this._options.plots.opacity,
-                    startAngle: angleScale(i),
-                    endAngle: angleScale(i + 1),
-                    r: 0,
-                    data: e.values[i],
-                }
-            });
-            // larger slice are drawn first
-            array.sort((a, b) => b.data.y - a.data.y);
-
-            return {
-                dimension: d,
-                slice: array
-            }
-        });
-
         const finalState = this._getDimension().values.map((d, i) => {
             let array = this._data.nested.map(e=> {
                 return {
@@ -101,60 +76,29 @@ class Rose extends AbstractStackedCartesianChart {
             }
         });
 
-
-
-        // cache finalState as the initial state of next animation call
-        // this.previousState = finalState;
-
         let that = this;
         const ctx = that._frontContext;
         const opt = that._options;
 
 
-        const bloom = i=> {
-            let _init = finalState.slice(0, i+1);
-            const _target = finalState.slice(0, i+1);
-            _init[i] = initialState[i];
 
-            return animateStates(_init,
-                _target,
-                350,
+
+        if (!this.previousState) {
+            const initialState = getInitialState(this._getDimension().values, this._data.nested, opt, c);
+            drawBloomingRose(initialState, finalState, ctx, opt);
+        } else {
+            animateStates(this.previousState,
+                finalState,
+                opt.animation.duration.update,
                 ctx,
-                opt);
+                opt).then(res=>{
+            });
         }
 
-        finalState.reduce((acc, cur, i)=> {
-                return acc = acc.then(res=> bloom(i))
-            }, Promise.resolve());
-
-        // animateStates(initData, data, opt.animation.duration.update, ctx, opt);
-        // drawCanvas(ctx, finalState, opt);
+        this.previousState = finalState;
 
 
-        //
-        // animateStates(initialState,
-        //     finalState,
-        //     opt.animation.duration.update,
-        //     ctx,
-        //     opt).then(res=>{
-        //     // that._voronoi = applyVoronoi(ctx, opt, finalState.reduce((acc, p)=>{
-        //     //     acc = acc.concat(p.values.map(d=>{
-        //     //         return {
-        //     //             s: p.key,
-        //     //             label: that._getDimensionVal(d.data),
-        //     //             metric: d.data[p.key],
-        //     //             x: d.r * Math.sin(d.angle) + opt.chart.width / 2,
-        //     //             y: opt.chart.height - (d.r * Math.cos(d.angle) + opt.chart.height / 2),
-        //     //             c: p.c,
-        //     //             d: d,
-        //     //             data: d.data
-        //     //         }
-        //     //     }));
-        //     //     return acc;
-        //     // }, []));
-        //
-        //
-        // });
+
     }
 
     createOptions(_userOpt) {
