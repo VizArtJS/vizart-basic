66070732532cbf3768ad8b0c5661e0e137fb36a2
Xing Yun <ink.xing.yun@gmail.com>
2017-10-27 15:25:14 -0400

    enable/disable animation

--- src/modal/Rose/tween-states.js
+++ src/modal/Rose/tween-states.js
@@ -4,20 +4,28 @@ import { easeCubic } from 'd3-ease';
 import drawCanvas from './draw-canvas';
 
 const animateStates = (initialState, finalState, duration, context, opt)=> {
-    return new Promise((resolve, reject)=> {
-        const interpolateParticles = interpolateArray(initialState, finalState);
+    if (opt.animation.enabled === false) {
+        return new Promise((resolve, reject)=> {
+            drawCanvas(context, finalState, opt);
 
-        const batchRendering = timer( (elapsed)=> {
-            const t = Math.min(1, easeCubic(elapsed / duration));
-
-            drawCanvas(context, interpolateParticles(t), opt);
-
-            if (t === 1) {
-                batchRendering.stop();
-                resolve(finalState);
-            }
+            resolve(finalState);
         });
-    });
+    } else {
+        return new Promise((resolve, reject)=> {
+            const interpolateParticles = interpolateArray(initialState, finalState);
+
+            const batchRendering = timer( (elapsed)=> {
+                const t = Math.min(1, easeCubic(elapsed / duration));
+
+                drawCanvas(context, interpolateParticles(t), opt);
+
+                if (t === 1) {
+                    batchRendering.stop();
+                    resolve(finalState);
+                }
+            });
+        });
+    }
 }
 
 export default animateStates;
--- src/modal/StackedBar/tween-states.js
+++ src/modal/StackedBar/tween-states.js
@@ -5,23 +5,33 @@ import { easeCubic } from 'd3-ease';
 import drawCanvas from './draw-canvas';
 
 const animateStates = (initialState, finalState, duration, context, opt)=> {
-    return new Promise((resolve, reject)=> {
-        const interpolateParticles = interpolateArray(initialState, finalState);
+    if (opt.animation.enabled === false) {
+        return new Promise((resolve, reject)=> {
+            drawCanvas(context, finalState, opt);
 
-        const batchRendering = timer( (elapsed)=> {
-            const t = Math.min(1, easeCubic(elapsed / duration));
-
-            drawCanvas(context,
-                interpolateParticles(t),
-                opt);
-
-            if (t === 1) {
-                batchRendering.stop();
-                // final state as result
-                resolve(finalState);
-            }
+            resolve(finalState);
         });
-    });
+    } else {
+        return new Promise((resolve, reject)=> {
+            const interpolateParticles = interpolateArray(initialState, finalState);
+
+            const batchRendering = timer( (elapsed)=> {
+                const t = Math.min(1, easeCubic(elapsed / duration));
+
+                drawCanvas(context,
+                    interpolateParticles(t),
+                    opt);
+
+                if (t === 1) {
+                    batchRendering.stop();
+                    // final state as result
+                    resolve(finalState);
+                }
+            });
+        });
+    }
+
+
 }
 
 export default animateStates;
