a908e6e6479de6729b843b4d8151ec82b6513a84
Xing Yun <ink.xing.yun@gmail.com>
2018-03-16 22:07:38 -0400

    more concise code

--- src/helper/composers.js
+++ src/helper/composers.js
@@ -4,24 +4,20 @@ import createCartesianStackedOpt from '../options/createCartesianStackedOpt';
 import { processCartesianData, processStackedData } from '../data';
 import createCartesianOpt from '../options/createCartesianOpt';
 
-const colorBySeries = (colorOpt, data, opt) => {
-  const distinct = data && data.nested ? data.nested.length : 0;
-  return categoricalColor(opt.color.scheme, distinct);
-};
-
 const stackedComposer = ChartOpt => ({
   opt: opt => createCartesianStackedOpt(ChartOpt, opt),
   data: processStackedData,
-  color: colorBySeries,
+  color: (colorOpt, data, opt) => {
+      const distinct = data && data.nested ? data.nested.length : 0;
+      return categoricalColor(opt.color.scheme, distinct);
+  },
 });
-
-const colorComposer = (colorOpt, data, opt) =>
-  genericColor(colorOpt, data.map(d => d[opt.data.y[0].accessor]));
 
 const standardComposer = ChartOpt => ({
   opt: opt => createCartesianOpt(ChartOpt, opt),
   data: processCartesianData,
-  color: colorComposer,
+  color: (colorOpt, data, opt) =>
+      genericColor(colorOpt, data.map(d => d[opt.data.y[0].accessor])),
 });
 
 export { stackedComposer, standardComposer };
